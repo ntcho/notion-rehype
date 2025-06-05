da177dfcd9de1e6cc3b37bba726de08fc9646d11
Nathan Cho <ntcho13@gmail.com>
2025-06-05 18:09:40 -0400

    feat: enhance video handler to support YouTube embeds

--- src/handlers/video.ts
+++ src/handlers/video.ts
@@ -8,12 +8,36 @@ const handler = (context: Context, block: GetBlock<'video'>) => {
 
   const blockClass = notionPrefixFactory(context)(block.type);
 
-  const hast = h('div', { className: [blockClass] }, [
-    h('video', { src: url, controls: true, ...fileAttr }, []),
-  ]);
+  const youtubeMatch =
+    data.type === 'file' ? null : YOUTUBE_URL_REGEX.exec(data.external.url);
+
+  const video =
+    youtubeMatch === null
+      ? h('video', { src: url, controls: true, ...fileAttr }, [])
+      : h(
+          'iframe',
+          { src: `https://youtube.com/embed/${youtubeMatch[5]}`, ...fileAttr },
+          []
+        );
+
+  const hast = h('div', { className: [blockClass] }, [video]);
   addCaptionToHast(context, hast, data.caption);
 
   return hast;
 };
 
 export default handler;
+
+/**
+ * From https://stackoverflow.com/a/37704433/4524257
+ *
+ * Capture groups:
+ * 1. protocol
+ * 2. subdomain
+ * 3. domain
+ * 4. path
+ * 5. video code
+ * 6. query string
+ */
+const YOUTUBE_URL_REGEX =
+  /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$/;
