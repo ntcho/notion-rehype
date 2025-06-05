1079529a214345db99e6a8e490eff848aa60a0a7
Nathan Cho <ntcho13@gmail.com>
2025-06-05 18:09:31 -0400

    feat: add flex width for custom width columns

--- src/handlers/column.ts
+++ src/handlers/column.ts
@@ -7,7 +7,14 @@ const handler = (context: Context, block: GetBlock<'column'>) => {
 
   const blockClass = notionPrefixFactory(context)(block.type);
 
-  const hast = h('section', { className: [blockClass] }, []);
+  const hast = h(
+    'section',
+    {
+      className: [blockClass],
+      style: `flex: ${data.width_ratio ? data.width_ratio.toFixed(2) : 1}`,
+    },
+    []
+  );
   addTasksToAddDirectChildren(context, hast, data.children);
 
   return hast;
